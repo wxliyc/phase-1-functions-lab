11d521a9299b9a0fdc5112de0c0d9e01f491cac0
Ethan Lee <wxliyc@gmail.com>
2022-02-21 11:07:40 -0600

    commit tree 882dace0d0ad5361f5d1aadff0c7f5cebdcb6bae

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if(someValue > 42){
+    return someValue - 42
+    }
+    else {
+        return 42 - someValue
+    }
+  }
+
+
+
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue) * 264
+}
+
+
+
+
+
+function distanceTravelledInFeet(start, destination) {
+   if(destination > start){
+    return (destination - start) * 264
+   }
+    else {
+        return (start - destination) * 264
+    }
+   } 
+
+
+function calculatesFarePrice(start, destination) {
+    if(distanceTravelledInFeet(start, destination) <= 400){
+        return 0
+    }
+    if(400 < distanceTravelledInFeet(start, destination) && distanceTravelledInFeet(start, destination)<= 2000){
+        return 0.02 * (distanceTravelledInFeet(start, destination) - 400)
+    }
+    if(2000 < distanceTravelledInFeet(start, destination) && distanceTravelledInFeet(start, destination) <= 2500){
+        return 25
+    }
+    if(distanceTravelledInFeet(start, destination) > 2500){
+        return 'cannot travel that far'
+    }
+  }
